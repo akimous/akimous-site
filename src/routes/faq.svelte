2cf11773d9c686e2404855d68c1772b9fc74a7b1
Yu-Ann Chen <red8012@gmail.com>
2019-11-23 09:17:34 +0800

    lint; change progress bar animation-fill-mode to forwards

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -1,7 +1,6 @@
 <div class="single-column-container">
     <section>
         <h1>FAQ</h1>
-        <h2></h2>
     </section>
 </div>
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,24 +13,24 @@
 <script>
     import { onMount, tick } from 'svelte'
     
-	import Hero from '../features/Hero.svelte'
-	import Prediction from '../features/Prediction.svelte'
-	import ExtraPrediction from '../features/ExtraPrediction.svelte'
-	import Documentation from '../features/Documentation.svelte'
-	import LayeredKeyboardControl from '../features/LayeredKeyboardControl.svelte'
-	import RealtimeFormatter from '../features/RealtimeFormatter.svelte'
-	import Console from '../features/Console.svelte'
-	import Macro from '../features/Macro.svelte'
-	import LinterIntegration from '../features/LinterIntegration.svelte'
+    import Hero from '../features/Hero.svelte'
+    import Prediction from '../features/Prediction.svelte'
+    import ExtraPrediction from '../features/ExtraPrediction.svelte'
+    import Documentation from '../features/Documentation.svelte'
+    import LayeredKeyboardControl from '../features/LayeredKeyboardControl.svelte'
+    import RealtimeFormatter from '../features/RealtimeFormatter.svelte'
+    import Console from '../features/Console.svelte'
+    import Macro from '../features/Macro.svelte'
+    import LinterIntegration from '../features/LinterIntegration.svelte'
     
     export let ready = false
     
     onMount(async () => {
-		await tick()
+        await tick()
         requestAnimationFrame(() => {
             requestAnimationFrame(() => {
                 ready = true
             })
         })
-	})
+    })
 </script>
